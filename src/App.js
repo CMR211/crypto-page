cabe068b9b8cd0c6a00ca1a6534912d11a0702c9
cmr211 <bartosz.surma211@gmail.com>
2022-01-26 21:36:03 +0100

    Refactor 2

--- src/App.js
+++ src/App.js
@@ -6,7 +6,9 @@ import './main.css'
 
 import axios from 'axios'
 
-function storageAvailable(type) {
+function isStorageAvailable(type) {
+    // checking if there is a storage available
+    // type either 'local-storage' or 'session-storage'
     try {
         var storage = window[type],
             x = '__storage_test__'
@@ -38,12 +40,10 @@ function saveToLS(object) {
         localStorage.setItem(object.name, JSON.stringify(newObject))
     }
 }
-// -----------------------------------------------------------
-function App() {
-    // ---------------------------------------------------
-    // ------------ STOCK FETCHING -----------------------
-    const [popularStockData, setPopularStockData] = React.useState()
-    const stockOptions = {
+
+async function fetchPopularStocks(callbackFn) {
+    // callback: setPopularStocks(stocks)
+    const popularStocksFetchOptions = {
         method: 'GET',
         url: 'https://stock-data-yahoo-finance-alternative.p.rapidapi.com/v6/finance/quote',
         params: { symbols: 'aapl,gme,msft,nio,tsla,pltr,amc,intc' },
@@ -55,50 +55,130 @@ function App() {
             'Access-Control-Allow-Origin': '*',
         },
     }
-    async function fetchStockData() {
-        try {
-            const response = await axios.request(stockOptions)
-            setTimeout(() => {
-                const array = response.data.quoteResponse.result
-                const stocks = []
-                array.forEach((item) => {
-                    const stock = {
-                        symbol: item.symbol,
-                        name: item.shortName,
-                        price: item.regularMarketOpen,
-                        change: item.regularMarketChangePercent,
-                    }
-                    stocks.push(stock)
-                })
-                setPopularStockData(stocks)
-                // format:
-                // change: -1.0347117
-                // name: "Apple Inc."
-                // price: 166.98
-                // symbol: "AAPL"
-            }, 300)
-        } catch (e) {
-            console.log('Fetching Stock Data failed.', e)
-        }
+    try {
+        const response = await axios.request(popularStocksFetchOptions)
+        setTimeout(() => {
+            const array = response.data.quoteResponse.result
+            const stocks = []
+            array.forEach((item) => {
+                const stock = {
+                    symbol: item.symbol,
+                    name: item.shortName,
+                    price: item.regularMarketOpen,
+                    change: item.regularMarketChangePercent,
+                }
+                stocks.push(stock)
+            })
+            callbackFn(stocks)
+            // format:
+            // change: -1.0347117
+            // name: "Apple Inc."
+            // price: 166.98
+            // symbol: "AAPL"
+        }, 500)
+    } catch (e) {
+        console.error('Fetching Popular Stocks failed.', e)
+    }
+}
+
+async function fetchPopularCryptos(callbackFn) {
+    const popularCryptosFetchOptions = {
+        coins: 'bitcoin,ethereum,cardano,ripple,binancecoin,solana,polkadot,dogecoin',
+        url: `https://api.coingecko.com/api/v3/simple/price?ids=${popularCryptosFetchOptions.coins}&vs_currencies=usd&include_market_cap=true&include_24hr_change=true`,
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            Accept: 'json/text',
+            'Content-type': 'json/text',
+        },
+    }
+    try {
+        const response = await fetch(
+            popularCryptosFetchOptions.url,
+            popularCryptosFetchOptions.headers
+        )
+        const json = await response.json()
+        setTimeout(() => callbackFn(json), 500)
+    } catch (e) {
+        console.error('Fetching Popular Cryptos failed.', e)
+    }
+}
+
+async function fetchCryptoList(callbackFn) {
+    const cryptoListFetchOptions = {
+        url: 'https://api.coingecko.com/api/v3/coins/list',
+        headers: {
+            'Access-Control-Allow-Origin': '*',
+            Accept: 'json/text',
+            'Content-type': 'json/text',
+        },
+    }
+    try {
+        const response = await fetch(
+            cryptoListFetchOptions.url,
+            cryptoListFetchOptions.headers
+        )
+        const json = await response.json()
+        setTimeout(() => {
+            callbackFn(json)
+        }, 1000) // set to 1s since the list is huge
+    } catch (e) {
+        console.error('Fetching Coins List failed.', e)
+    }
+}
+
+async function fetchStocks(stocks, callbackFn) {
+    const options = {
+        method: 'GET',
+        url: 'https://alpha-vantage.p.rapidapi.com/query',
+        params: {
+            interval: '5min',
+            function: 'GLOBAL_QUOTE',
+            symbol: stocks.join(','),
+            datatype: 'json',
+            output_size: 'compact',
+        },
+        headers: {
+            'x-rapidapi-host': 'alpha-vantage.p.rapidapi.com',
+            'x-rapidapi-key':
+                '044f218bf5msh695d9c96153799bp1c58eejsn7e16c29df7a0',
+        },
     }
 
+    axios
+        .request(options)
+        .then(function (response) {
+            console.log(response.data)
+        })
+        .catch(function (error) {
+            console.error(error)
+        })
+}
+
+// -----------------------------------------------------------
+function App() {
+    // Fetching popular stocks and popular cryptos
+    const [popularStocks, setPopularStocks] = React.useState()
+    const [popularCryptos, setPopularCryptos] = React.useState()
     React.useEffect(() => {
-        fetchStockData()
+        fetchPopularStocks(setPopularStocks)
+        fetchPopularCryptos(setPopularCryptos)
     }, [])
 
-    // ---------------------------------------------------
-    // --------- CRYPTO FETCHING -------------------------
-
-    // cryptocurrency data state
-    const [popularCryptoData, setPopularCryptoData] = React.useState()
+    // Fetching cryptos and stocks list of available assets
     const [cryptoList, setCryptoList] = React.useState()
+    const [stocksList, setStocksList] = React.useState()
+    React.useEffect(() => {
+        // fetchPopularStocks(setPopularStocks)
+        fetchCryptoList(setCryptoList)
+    }, [])
+
     const [personalCrypto, setPersonalCrypto] = React.useState()
     const [personalAssets, setPersonalAssets] = React.useState(null)
 
     // ---------------------------------------------------
-    // get personal coins list form localstorage
+    // Get personal coins list form localstorage
     React.useEffect(() => {
-        if (!storageAvailable('localStorage')) return
+        if (!isStorageAvailable('localStorage')) return
         const array = []
         for (let i = 0; i < localStorage.length; i++) {
             const obj = JSON.parse(localStorage.getItem(localStorage.key(i)))
@@ -115,61 +195,13 @@ function App() {
 
     // ---------------------------------------------------
     // fetch options for cryptocurrency API call
-    const fetchOptionsCrypto = {
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            Accept: 'json/text',
-            'Content-type': 'json/text',
-        },
-    }
+
     // ---------------------------------------------------
 
     // Fetching popular cryptocurrency
     React.useEffect(() => {
         // fetch cryptocurrency data (function declaration)
-        async function fetchPopularCryptoData(token) {
-            try {
-                const response = await fetch(
-                    `https://api.coingecko.com/api/v3/simple/price?ids=${token}&vs_currencies=usd&include_market_cap=true&include_24hr_change=true`,
-                    fetchOptionsCrypto.headers
-                )
-                const json = await response.json()
-                setTimeout(() => setPopularCryptoData(json), 300)
-            } catch (e) {
-                console.log('Fetching Stock Data failed.', e)
-            }
-        }
-
-        // fetch cryptocurrency data (function invoke)
-        fetchPopularCryptoData(
-            'bitcoin,ethereum,cardano,ripple,binancecoin,solana,polkadot,dogecoin'
-        )
-
         //todo: stock data: financialmodelingprep ... fmp
-    }, [])
-    // ---------------------------------------------------
-
-    // ---------------------------------------------------
-    // fetch Coins List
-    React.useEffect(() => {
-        // fetch cryptocurrency data (function declaration)
-        async function fetchCoinsList() {
-            try {
-                const response = await fetch(
-                    'https://api.coingecko.com/api/v3/coins/list',
-                    fetchOptionsCrypto.headers
-                )
-                const json = await response.json()
-                setTimeout(() => {
-                    setCryptoList(json)
-                }, 1000)
-            } catch (e) {
-                console.error('Fetching Coins List failed.', e)
-            }
-        }
-
-        // fetch cryptocurrency coins list (function invoke)
-        fetchCoinsList()
     }, [])
     // ---------------------------------------------------
 
